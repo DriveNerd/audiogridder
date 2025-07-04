e3288a3e7f312f36c6545e35ece483fd89a424a8
DriveNerd <Bolf90@protonmail.com>
2025-07-04 05:55:53 +0200

    Add Plugin/Source/Images.hpp 1751601352886

---
+++ Plugin/Source/Images.hpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Andreas Pohl
+ * Licensed under MIT (https://github.com/apohl79/audiogridder/blob/master/COPYING)
+ *
+ * Author: Andreas Pohl
+ */
+
+#ifndef Images_hpp
+#define Images_hpp
+
+#include <JuceHeader.h>
+
+class Images {
+  public:
+    static const char* server_png;
+    static const int server_pngSize;
+    static const char* settings_png;
+    static const int settings_pngSize;
+    static const char* logo_png;
+    static const int logo_pngSize;
+    static const char* cpu_png;
+    static const int cpu_pngSize;
+    static const char* pluginlogo_png;
+    static const int pluginlogo_pngSize;
+};
+
+#endif  // Images_hpp
