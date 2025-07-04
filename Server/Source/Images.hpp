0bd57419b4aba5207f74350a88f9811208f679d9
DriveNerd <Bolf90@protonmail.com>
2025-07-04 05:55:14 +0200

    Add Server/Source/Images.hpp 1751601314321

---
+++ Server/Source/Images.hpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Andreas Pohl
+ * Licensed under MIT (https://github.com/apohl79/audiogridder/blob/master/COPYING)
+ *
+ * Author: Andreas Pohl
+ */
+
+#ifndef Images_hpp
+#define Images_hpp
+
+#include <JuceHeader.h>
+
+class Images {
+  public:
+    static const char* serverinv_png;
+    static const int serverinv_pngSize;
+    static const char* logotxt_png;
+    static const int logotxt_pngSize;
+    static const char* logo_png;
+    static const int logo_pngSize;
+    static const char* logowintray_png;
+    static const int logowintray_pngSize;
+    static const char* logowintraylight_png;
+    static const int logowintraylight_pngSize;
+};
+
+#endif  // Images_hpp
