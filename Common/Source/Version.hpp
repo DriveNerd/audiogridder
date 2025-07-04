72809f8402f96eec3bd26ee5b7014c3b4cff02c6
DriveNerd <Bolf90@protonmail.com>
2025-07-04 05:56:16 +0200

    Add Common/Source/Version.hpp 1751601376387

---
+++ Common/Source/Version.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 Andreas Pohl
+ * Licensed under MIT (https://github.com/apohl79/audiogridder/blob/master/COPYING)
+ *
+ * Author: Andreas Pohl
+ */
+
+#ifndef Version_hpp
+#define Version_hpp
+
+#define AUDIOGRIDDER_VERSION "dev-build"
+#define AUDIOGRIDDER_VERSIONW L"dev-build"
+#define AUDIOGRIDDER_VERSION_NUM "1.2.0"
+#define AUDIOGRIDDER_VERSION_NUMW L"1.2.0"
+#define AUDIOGRIDDER_BUILD_DATE ""
+
+#endif /* Version_hpp */
