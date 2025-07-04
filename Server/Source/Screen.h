fac5a12724399cb1e0b020a07cf2b6408250b44a
DriveNerd <Bolf90@protonmail.com>
2025-07-04 05:54:57 +0200

    Add Server/Source/Screen.h 1751601296923

---
+++ Server/Source/Screen.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2020 Andreas Pohl
+ * Licensed under MIT (https://github.com/apohl79/audiogridder/blob/master/COPYING)
+ *
+ * Author: Andreas Pohl
+ */
+
+#ifndef Screen_h
+#define Screen_h
+
+#include <juce_graphics/juce_graphics.h>
+
+using namespace juce;
+
+namespace e47 {
+
+std::shared_ptr<Image> captureScreenNative(Rectangle<int> rect);
+
+#ifdef JUCE_MAC
+int getCaptureDeviceIndex();
+void askForScreenRecordingPermission();
+bool askForAccessibilityPermission();
+#endif
+
+}  // namespace e47
+
+#endif /* Screen_h */
